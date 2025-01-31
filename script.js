d3d1fbd942695b45decab22e7082356bd468404b
Tanu Upadhyay <96697977+tanucs@users.noreply.github.com>
2025-01-31 21:34:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
+const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
+const header = document.querySelector('.header.container');
+const skillBars = document.querySelectorAll('.skill-bar .skill-progress');
+
+hamburger.addEventListener('click', () => {
+	hamburger.classList.toggle('active');
+	mobile_menu.classList.toggle('active');
+});
+
+document.addEventListener('scroll', () => {
+	var scroll_position = window.scrollY;
+	if (scroll_position > 250) {
+		header.style.backgroundColor = '#29323c';
+	} else {
+		header.style.backgroundColor = 'transparent';
+	}
+});
+
+menu_item.forEach((item) => {
+	item.addEventListener('click', () => {
+		hamburger.classList.toggle('active');
+		mobile_menu.classList.toggle('active');
+	});
+});
+// Function to animate skill progress bars
+function animateSkills() {
+    skillBars.forEach(bar => {
+        const percentage = bar.getAttribute('data-percentage'); // Get the percentage from the data attribute
+        bar.style.width = percentage;
+    });
+}
+
+// Call the animation function when the page loads
+window.addEventListener('load', animateSkills);
